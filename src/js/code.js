d002e1b6cab8e6e608404be478e756a675f878d7
Yasser Chenik <yasserchenik218@gmail.com>
2023-11-05 05:07:56 +0100

    Completed

--- src/js/code.js
+++ src/js/code.js
@@ -1,6 +1,9 @@
 const display = $('#map');
 var map = [];
 
+if(isDarkMode()){
+    swapMode($(`#switchForBackGround`));
+}
 
 function initApp(){
 
@@ -13,12 +16,19 @@ function initApp(){
     
         var pointGraphic = $('<span>') ;
         
-        pointGraphic.text(point.nom);
+        if( point.nom.length > 1 ){
+            pointGraphic.html(`
+                <span class="ticker-content">${point.nom}</div>
+            `);
+        }else{
+            pointGraphic.html(`${point.nom}`);
+        }
         pointGraphic.prop('id',`ptn_${point.nom}`);
         pointGraphic.data('sommet',point);
         pointGraphic.addClass('cercle');
         pointGraphic.addClass('draggable');
-        pointGraphic.on('dblclick',()=>{setNewPointOrRunAlgo(point)});
+        pointGraphic.on('dblclick',(e)=>{setNewPointOrRunAlgo(point)});
+        pointGraphic.on('click',(e)=>{e.stopPropagation();});
         display.append(pointGraphic);
     
         for (const link of point.links) {
@@ -114,6 +124,18 @@ function initApp(){
 
 function runAlgoRithmDikstra( sommetDepart , sommetFin ){
     
+    map = map.sort((a,b)=>{
+        if(a.nom>b.nom){
+            return 1;
+        }
+        else if(a.nom<b.nom){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    });
+
     $('.selected').removeClass('selected');
     $('.reversed').removeClass('reversed');
 
@@ -227,7 +249,7 @@ function runAlgoRithmDikstra( sommetDepart , sommetFin ){
                     id = `#path_${report.source.nom}_${report.sommet.nom}`;
                     $(id).addClass('selected');
                 }
-                console.log(report.source.nom);               
+                
                 recursivelyGivePath(report.source);
                 break ;
             }
@@ -255,19 +277,320 @@ function setNewPointOrRunAlgo(Point2){
         $(`#ptn_${Point1.nom}`).addClass('stoping_section'); 
     }
 }
-
+$(`#map`).on('click',()=>{
+    Point1 = null ;
+    Point2 = null ;
+    $('.stoping_section').removeClass('stoping_section');
+}); 
 
 // UI way to add buttons and links
 
-function editMap() {
-    $('#questionnaireApp').modal('show');
+var counterOfDisplays       = 0     ,
+    objectOfCreation        = {}    ,
+    inMiddleOfCreation      = false ;
+
+    
+var counterOfSecondDisplays = 0     ,
+    objectOfSecondCreation  = {}    ,
+    inMiddleOfSecondCreation= false ;
+
+
+$(document).ready(function () {
+    $(`#nom_point_pour_creation`).on('input',()=>{
+        if(!inMiddleOfCreation){
+            inMiddleOfCreation = true;
+            
+
+            counterOfDisplays ++;
+
+            let btnID = counterOfDisplays + 0;
+        
+            var input = $(`<input>`);
+            input.prop('id',`input_point_${btnID}`);
+            input.prop('value',$(`#nom_point_pour_creation`).val());
+            input.addClass('form-control point-css');
+            input.on('input',(e)=>{
+                objectOfCreation [btnID] = e.currentTarget.value ;
+            });
+            
+        
+            var row = $(`<tr>`) ,
+                btn = $(`<btn>`);
+        
+            btn.addClass('btn btn-danger w-100 text-center btn-sm p-1 fs-5');
+            btn.text('x');
+            btn.on('click',()=>{
+                $(`#rm_tr_${btnID}`).remove();
+                delete objectOfCreation [btnID];
+            });
+            row.prop('id',`rm_tr_${btnID}`);
+                
+            var td1 = $(`<td>`).append(input),
+                td2 = $(`<td>`).append(btn) ;
+            row.append(td1);
+            row.append(td2);
+        
+            $(`#points_placeholder`).append(row) ;
+            input.trigger('focus');
+        
+            objectOfCreation [btnID] = $(`#nom_point_pour_creation`).val() ;
+            $(`#nom_point_pour_creation`).val('');
+
+            resetPointsSelect();
+
+            inMiddleOfCreation = false;
+        }
+    });
+    $(`#link_distance_pour_creation`).on('input',()=>{
+        if(!inMiddleOfSecondCreation){
+            inMiddleOfSecondCreation = true;
+            
+
+            
+            
+            counterOfSecondDisplays ++;
+
+            let btnID = counterOfSecondDisplays + 0;
+        
+            var input = $(`<input>`);
+            input.prop('id',`input_link_${btnID}`);
+            input.prop('value',$(`#link_distance_pour_creation`).val());
+            input.addClass('form-control');
+            input.on('input',(e)=>{
+                objectOfSecondCreation [btnID] = e.currentTarget.value ;
+            });
+            
+        
+            var row = $(`<tr>`) ,
+                btn = $(`<btn>`);
+        
+            btn.addClass('btn btn-danger w-100 text-center btn-sm p-1 fs-5');
+            btn.text('x');
+            btn.on('click',()=>{
+                $(`#rm_tr_select_${btnID}`).remove();
+                delete objectOfSecondCreation [btnID];
+            });
+            row.prop('id',`rm_tr_select_${btnID}`);
+                
+            var td1 = $(`<td>`).append(input),
+                td2 = $(`<td>`).append(btn) ;
+
+            var td3 = $(`<td>`);
+            var td4 = $(`<td>`);
+            var list3 = $(`<select>`);
+            var list4 = $(`<select>`);
+
+            list3.prop('id',`select_for_linking_start_${btnID}`);
+            list4.prop('id',`select_for_linking_end_${btnID}`);
+            list3.addClass('form-control list_option_buttons');
+            list4.addClass('form-control list_option_buttons');
+
+            
+
+            td3.append(list3);
+            td4.append(list4);
+
+            row.append(td3);
+            row.append(td4);
+            row.append(td1);
+            row.append(td2);
+        
+            $(`#links_placeholder`).append(row) ;
+            input.trigger('focus');
+        
+            objectOfSecondCreation [btnID] = $(`#link_distance_pour_creation`).val() ;
+            $(`#link_distance_pour_creation`).val('');
+
+            resetPointsSelect();
+
+            inMiddleOfSecondCreation = false;
+        }
+    });
+});
+
+function retourEtap1(){
+
+    $(`#section_part1`).removeClass('d-none');
+    $(`#section_part2`).addClass('d-none');
+
+    $('#btn_Suivant').removeClass('d-none');
+    $('#btn_Precedant').addClass('d-none');
+    $('#btn_Generate_Map').addClass('d-none');
 }
+
+function openHelperMenu(){
+    $('#helpMenu').modal('show');
+}
+
+function editMap() {
+    
+    counterOfDisplays       = 0     ;
+    objectOfCreation        = {}    ;
+    counterOfSecondDisplays = 0     ;
+    objectOfSecondCreation  = {}    ;
+
+    $('#questionnaireApp').modal('show');
+    $(`#points_placeholder`).empty();
+    $(`#links_placeholder`).empty();
+    
+    $(`#nom_point_pour_creation`).val('');
+    $(`#link_distance_pour_creation`).val('');
+    
+    $(`#section_part1`).removeClass('d-none');
+    $(`#section_part2`).addClass('d-none');
+
+    $('#btn_Suivant').removeClass('d-none');
+    $('#btn_Precedant').addClass('d-none');
+    $('#btn_Generate_Map').addClass('d-none');
+
+    $(`.is-invalid`).removeClass('is-invalid');
+    // create all points from map 
+}
+
+
+function finEtape1(){
+    $(`.is-invalid`).removeClass('is-invalid');
+
+    var isValid = true ;
+
+    for (const [id,value] of Object.entries(objectOfCreation)) {
+
+        if( value == '' || !isAlphaNumeric(value) ){
+            $(`#input_point_${id}`).addClass('is-invalid');
+            isValid = false ;
+        }
+        
+    }
+
+
+    if( Object.keys(objectOfCreation).length < 2 ){
+        isValid = false ;
+        showErrorMessage("Veuillez créer plus d'un seul sommet !");
+    }
+
+    if(isValid){
+
+        $('#btn_Suivant').addClass('d-none');
+        $('#btn_Precedant').removeClass('d-none');
+        $('#btn_Generate_Map').removeClass('d-none');
+
+
+        $(`#section_part1`).addClass('d-none');
+        $(`#section_part2`).removeClass('d-none');
+
+        $(`#link_distance_pour_creation`).val('');
+        
+        resetPointsSelect();
+
+    }
+}
+
 
 
 function generateMap(){
     // generate the buttons and the links using jquery elements with the data that the user added
+    $(`.is-invalid`).removeClass('is-invalid');
+
+    var isValid = true 
+        alreadyShow = [];
+
+    for (const [id,value] of Object.entries(objectOfSecondCreation)) {
+
+        var hash = '' ;        
+        if($(`#select_for_linking_start_${id}`).val() < $(`#select_for_linking_end_${id}`).val()){
+            hash = `${$(`#select_for_linking_start_${id}`).val()}_${$(`#select_for_linking_end_${id}`).val()}`;
+        }else{
+            hash = `${$(`#select_for_linking_end_${id}`).val()}_${$(`#select_for_linking_start_${id}`).val()}`;
+        }
+        if( alreadyShow.indexOf(hash) != -1 ){
+            showErrorMessage("Veuillez ne pas répéter la même association !");
+            $(`#select_for_linking_start_${id}`).addClass('is-invalid');
+            $(`#select_for_linking_end_${id}`).addClass('is-invalid');
+            isValid = false ; 
+        }
+        alreadyShow.push(hash);
+
+
+
+        if( value == '' || !isNumeric(value) ){
+            $(`#input_link_${id}`).addClass('is-invalid');
+            isValid = false ;
+        }
+
+
+        if( $(`#select_for_linking_start_${id}`).val() == $(`#select_for_linking_end_${id}`).val() ){
+            showErrorMessage("Veuillez ne pas connecter un point avec lui même !");
+            $(`#select_for_linking_start_${id}`).addClass('is-invalid');
+            $(`#select_for_linking_end_${id}`).addClass('is-invalid');
+            isValid = false ;
+        }
+
+        if( $(`#select_for_linking_start_${id}`).val() == '' ){
+            $(`#select_for_linking_start_${id}`).addClass('is-invalid');
+            isValid = false ;
+        }
+
+        if( $(`#select_for_linking_end_${id}`).val() == '' ){
+            $(`#select_for_linking_end_${id}`).addClass('is-invalid');
+            isValid = false ;
+        }
+        
+    }
+
+    
+    if(Object.keys(objectOfSecondCreation).length < (Object.keys(objectOfCreation).length - 1) ){
+        isValid = false ;
+        showErrorMessage("Veuillez ne pas laissez un sommet isolé !");
+    }
+
+    if(isValid){
+
+        function findPointByValue(value){
+            for (const sommet of map) {
+                if(sommet.nom == value){
+                    return sommet ;
+                }
+            }
+        }
+
+        map = [];
+        
+        for (const [id,value] of Object.entries(objectOfCreation)) {
+            
+            map.push(new Sommet(value));
+
+        }
+    
+        for (const [id,distance] of Object.entries(objectOfSecondCreation)) {
+
+            var point1 = findPointByValue($(`#select_for_linking_start_${id}`).val()) ,
+                point2 = findPointByValue($(`#select_for_linking_end_${id}`).val())   ;
+
+            point1.addLink(point2,+distance);                
+            
+        }
+        
+        initApp();
+        
+        $('#questionnaireApp').modal('hide');
+    
+    }
 }
 
+function resetPointsSelect() {
+    for (const [id,point] of Object.entries(objectOfCreation)) {
+        for (const list of $('.list_option_buttons')) {
+            if( $(list).find(`option[value=${point}]`).length == 0 ){
+
+                $option = $('<option>') ;
+                $option.prop('value',point);
+                $option.text(point);
+                $(list).append($option);
+
+            }
+        }
+    }
+}
 
 // example starting
 
@@ -281,25 +604,25 @@ var A = new Sommet('A') ,
     O = new Sommet('O') ;
 
 
-A.addLink(B,5);
-A.addLink(D,10);
-A.addLink(E,6);
-D.addLink(E,3);
-D.addLink(F,1);
-D.addLink(G,4);
-D.addLink(C,7);
-E.addLink(F,5);
-F.addLink(G,2);
-G.addLink(C,3);
-B.addLink(C,5);
-B.addLink(O,5);
+A.addLink( B ,  5 ) ;
+A.addLink( D , 10 ) ;
+A.addLink( E ,  6 ) ;
+D.addLink( E ,  3 ) ;
+D.addLink( F ,  1 ) ;
+D.addLink( G ,  4 ) ;
+D.addLink( C ,  7 ) ;
+E.addLink( F ,  5 ) ;
+F.addLink( G ,  2 ) ;
+G.addLink( C ,  3 ) ;
+B.addLink( C ,  5 ) ;
+B.addLink( O ,  5 ) ;
 
 
-// fislina 
 
 map = [
     A,B,C,D,E,F,G,O
-]
+];
+
 // les points de a => z si non la direction de voyage sera ironne
 
 initApp();
--- utility-packages/js/commonFunctions.js
+++ utility-packages/js/commonFunctions.js
@@ -1,3 +1,21 @@
+function swapMode(checkbox) {
+  $(`[data-bs-theme]`).prop('data-bs-theme',checkbox.checked ? "dark":"light");
+  $(`[data-bs-theme]`).attr('data-bs-theme',checkbox.checked ? "dark":"light");
+
+  
+  if(checkbox.checked){
+    var btns = $(`.btn-light`);
+    btns.removeClass('btn-light');
+    btns.addClass('btn-dark');
+    $('#map').removeClass('dark');
+  }else{
+    var btns = $(`.btn-dark`);
+    btns.removeClass('btn-dark');
+    btns.addClass('btn-light');
+    $('#map').addClass('dark');
+  }
+}
+
 function objectInArrayByReference(object, array) {
     for (const obj of array) {
       if (object === obj) {
@@ -6,9 +24,12 @@ function objectInArrayByReference(object, array) {
     }
     return false;
 }
+function isDarkMode() {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
 
 
-  
+
 function connectTwoSpansInCenter(span1, span2, connectingSpan) {
   connectingSpan.css({
       transformOrigin: 'top left'
@@ -51,3 +72,39 @@ function connectTwoSpansInCenter(span1, span2, connectingSpan) {
     });
 
 }
+
+
+function isNumeric(str){
+  return /^\d+$/.test(str) ;
+}
+
+function isAlphaNumeric(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+};
+
+var showErrorMessage = (error)=>{
+  console.error(error);
+};
+$(document).ready(function () {
+
+  var toastEl = $('#error_toast')[0];
+  var toast = new bootstrap.Toast(toastEl);
+  $('#error_toast').on('click', function () {
+    toast.hide(); 
+  });
+  
+  showErrorMessage = (error) => {
+    $('#error_message').text(error);
+    toast.show();
+  };
+});
